08578fe8d897d449206f1f70d6fe14312ab8c2d4
Kerem Cesme <keremcesme@gmail.com>
2022-08-10 13:44:57 +0300

    Initial Commit

---
+++ vnvce/vnvceApp.swift
@@ -0,0 +1,17 @@
+//
+//  vnvceApp.swift
+//  vnvce
+//
+//  Created by Kerem Cesme on 10.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct vnvceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
